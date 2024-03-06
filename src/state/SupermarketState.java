1453f82d8d93e2b2777aedd7ed6c9ce165f08322
rehygaR <48217982+rehygaR@users.noreply.github.com>
2024-03-06 20:04:01 +0100

    Uppdaterat SupermarketState
    Gjort alla variabler privata, till och med FIFO kön. Dessa kan nås nu via en getter metod och ändras med en setter metod.

--- src/state/SupermarketState.java
+++ src/state/SupermarketState.java
@@ -21,31 +21,31 @@ public class SupermarketState extends SimState {
 	 * Tilltståndsvariabler / Statistik
 	 */
 	private int maxNumOfCustomers;
-	public int numOfCustomers; // Antal customers i butiken
+	private int numOfCustomers; // Antal customers i butiken
 	private int numCheckouts; // Ska tilldelas från vår runSim där detta anges
-	public int freeCheckouts; // Detta ska räknas ut genom att ta i akt på FIFO tillståndet och antal personer i snabbköpet
+	private int freeCheckouts; // Detta ska räknas ut genom att ta i akt på FIFO tillståndet och antal personer i snabbköpet
 	private double sumTimeFreeCheckouts; // Summering av tiden som det funnits lediga kassor. Slutas räknas då sista kunden betalat och lämnat snabbköpet. (Även om det kommer en ny kund som inte kommer in pga stängning)
-	public int numCustomersLeaving; // Antal kunder som handlat och lämnar snabbköpet (genomgått alla kund-händelser)
+	private int numCustomersLeaving; // Antal kunder som handlat och lämnar snabbköpet (genomgått alla kund-händelser)
 	private double sumTimeCustomersInQueue; // Summering av tiden som en kund stått i kön.
-	public int numCustomersMissed; // Missade kunder
-	public int totalQueuedCustomers;
+	private int numCustomersMissed; // Missade kunder
+	private int totalQueuedCustomers;
 	private double closingTime;
-	public int customerID;
+	private int customerID;
 	private double arrivalLambda;
 	private double pickupL;
 	private double pickupH;
 	private double paymentL;
 	private double paymentH;
-	public String currentEvent;
+	private String currentEvent;
 	
 	//private double currentTime; // Nuvarande tiden för hela simuleringen. Sluta räknas efter att sista kunden betalat.
 	
 	//protected double currentTimeCustomer = 0; // Nuvarande tiden för en specifik kund. Sluta räknas efter att sista kunden betalat.
 	
-	public ArrivalTime arrive;// = new ArrivalTime(4.0); // Ankomsttidskälla, 4.0 bytsut mot det man anger i runSim
-	public PickupTime pickup;// = new PickupTime(0.5,1.0); // Plocktidskälla, parametrar byts ut mot de angivna i runSim ( variabler)
-	public PaymentTime payment; // = new PaymentTime(0.5,1.0); // Betalningstidskälla, samma som ovan
-	public FIFO queue; //= new FIFO();
+//	public ArrivalTime arrive;// = new ArrivalTime(4.0); // Ankomsttidskälla, 4.0 bytsut mot det man anger i runSim
+//	public PickupTime pickup;// = new PickupTime(0.5,1.0); // Plocktidskälla, parametrar byts ut mot de angivna i runSim ( variabler)
+//	public PaymentTime payment; // = new PaymentTime(0.5,1.0); // Betalningstidskälla, samma som ovan
+	private FIFO queue; //= new FIFO();
 	
 	public SupermarketState(int antalKassor, int maxCustomers, double arrivalLambda,
 			double pickupL, double pickupH, double paymentL, double paymentH, double closingTime){ // Konstruktor, behövs detta?
@@ -54,8 +54,8 @@ public class SupermarketState extends SimState {
 		this.numOfCustomers = 0;
 		this.freeCheckouts = numCheckouts;
 		this.queue = new FIFO();
-		this.pickup = new PickupTime(pickupL, pickupH);
-		this.payment = new PaymentTime(paymentL, paymentH);
+//		this.pickup = new PickupTime(pickupL, pickupH);
+//		this.payment = new PaymentTime(paymentL, paymentH);
 		this.maxNumOfCustomers = maxCustomers;
 		this.sumTimeCustomersInQueue = 0.0;
 		this.numCustomersLeaving = 0;
@@ -72,7 +72,7 @@ public class SupermarketState extends SimState {
 	}
 	
 	/*
-	 * Getters
+	 * Getters & Setters
 	 */
 	public boolean open() {
 		if (getTime() >= closingTime) { // 10 placeholder, 10 står för tiden när butiken stänger
@@ -83,56 +83,73 @@ public class SupermarketState extends SimState {
 	}
 	
 	public int getMaxNumOfCustomers() {
-		return maxNumOfCustomers;
+		return this.maxNumOfCustomers;
 	}
 	
 	public int getCurrentCustomers() {
-		return numOfCustomers;
+		return this.numOfCustomers;
+	}
+	
+	public void setCurrrentCustomers(int numOfCustomers) {
+		this.numOfCustomers = numOfCustomers;
 	}
 	
 	public int getNumCheckouts() {
-		return numCheckouts;
+		return this.numCheckouts;
 	}
 	
 	public int getFreeCashiers() {
-		return freeCheckouts;
+		return this.freeCheckouts;
 	}
 	
-	public double getFreeCashierTime() {
-		sumTimeFreeCheckouts = (double) getFreeCashiers() * getTime();
-		return sumTimeFreeCheckouts;
+	public void setFreeCashiers(int freeCheckouts) {
+		this.freeCheckouts = freeCheckouts;
 	}
+	
+	public double getFreeCashierTime() { // Troligtvis fel beräkning!!!
+		this.sumTimeFreeCheckouts = (double) getFreeCashiers() * getTime();
+		return this.sumTimeFreeCheckouts;
+	}
+	
 	
 	public int getTotalPayingCustomers() {
-		return numCustomersLeaving;
+		return this.numCustomersLeaving;
+	}
+	
+	public void setTotalPayingCustomers(int numCustomersLeaving) {
+		this.numCustomersLeaving = numCustomersLeaving;
 	}
 	
 	
-	public double getQueueTime() {
-		sumTimeCustomersInQueue = (double) queue.size() * payment.getNextTime(currentTime);
-		return sumTimeCustomersInQueue;
+	public double getQueueTime() { // Troligtvis fel beräknad!!!!
+		this.sumTimeCustomersInQueue = (double) queue.size() * getPaymentTime();
+		return this.sumTimeCustomersInQueue;
 	}
 	
 	public int getMissedCustomers() {
-		return numCustomersMissed;
+		return this.numCustomersMissed;
 	}
 	
-	public int getQueuedCustomers() {
-		return totalQueuedCustomers;
+	public void setMissedCustomers(int numOfCustomersMissed) {
+		this.numCustomersMissed = numOfCustomersMissed;
+	}
+	
+	public int getQueuedCustomers() { // Returnerar antal Customers i FIFO kön (en int)
+		return this.queue.size();
 	}
 	
 	public String getStringQueue() { // Returnerar en sträng av kön, id på kund och vilken plats (index 0 = längst fram, FIFO)
-		return queue.toString();
+		return this.queue.toString();
 	}
 	
-	public double getAverageFreeCashierTime() { // Returnerar den tid i snitt som kassor är lediga
-		sumTimeFreeCheckouts = (double) getFreeCashiers() * currentTime;
-		return sumTimeFreeCheckouts;
+	public double getAverageFreeCashierTime() { // Returnerar den tid i snitt som kassor är lediga FEL!!!
+		this.sumTimeFreeCheckouts = (double) getFreeCashiers() * currentTime;
+		return this.sumTimeFreeCheckouts;
 	}
 	
-	public double getAverageQueueTime() { // Returnerar den tid i snitt som kunder får köa
-		sumTimeCustomersInQueue = (double) queue.size() * payment.getNextTime(currentTime);
-		return sumTimeCustomersInQueue;
+	public double getAverageQueueTime() { // Returnerar den tid i snitt som kunder får köa FEL!!!
+		this.sumTimeCustomersInQueue = (double) queue.size() * getPaymentTime();
+		return this.sumTimeCustomersInQueue;
 	}
 	
 	public double getFreeCashierPercentage() {
@@ -140,10 +157,10 @@ public class SupermarketState extends SimState {
 	}
 	
 	public double getClosingTime() {
-		return closingTime;
+		return this.closingTime;
 	}
 	
-	public double getArrivalTime() {
+	public double getArrivalTime() { // Ger ett nytt slumptal på AnkomstTid
 		return new ArrivalTime(arrivalLambda).getNextTime(currentTime);
 	}
 	
@@ -156,11 +173,31 @@ public class SupermarketState extends SimState {
 	}
 	
 	public int getCurrentCustomerID() {
-		return customerID;
+		return this.customerID;
 	}
 	
 	public String getCurrentEvent() {
-		return currentEvent;
+		return this.currentEvent;
+	}
+	
+	public void setCurrentEvent(String currentEvent) {
+		this.currentEvent = currentEvent;
+	}
+	
+	public FIFO getFIFO() { // Returnerar FIFO kön
+		return this.queue;
+	}
+	
+	public void addFIFO(Customer cr) { // Lägger till en Customer i FIFO kön
+		this.queue.add(cr);
+	}
+	
+	public void removeFirstFIFO() { // Tar bort det första Customern i FIFO kön
+		this.queue.removeFirst();
+	}
+	
+	public void isEmptyFIFO() { // Returnerar true eller false beroende på om kön är tom eller ej
+		this.queue.isEmpty();
 	}
 	
 //	public addTime(Event event) { // Lägger till tid beroende på vilket event
